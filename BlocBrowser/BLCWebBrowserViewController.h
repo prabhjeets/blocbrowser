992c7d26010c28b33054883197c840d6cc035fb4
PJ Singh <prabhjeets@yahoo.com>
2014-10-28 23:58:33 -0700

    Functional web browser

--- BlocBrowser/BLCWebBrowserViewController.h
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  BLCWebBrowserViewController.h
 //  BlocBrowser
 //
 //  Created by Prabhjeet Singh on 10/28/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface BLCWebBrowserViewController : UIViewController
 
 @end
-
