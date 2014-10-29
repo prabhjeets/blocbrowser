b69ab76559c76b9ee4eaad7f5b0fc2f1de16303a
PJ Singh <prabhjeets@yahoo.com>
2014-10-28 21:31:00 -0700

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Prabhjeet Singh on 10/28/14.
+//  Copyright (c) 2014 PJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Prabhjeet Singh on 10/28/14.
+//  Copyright (c) 2014 PJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
